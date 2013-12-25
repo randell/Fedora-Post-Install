c04652a071080ef6cebd72fae26edbd8c61a1a15
Randell Benavidez <josephrandell.benavidez@gmail.com>
2013-12-25 23:07:37 +1100

    Update fedora20-post_install.sh

--- fedora20-post_install.sh
+++ fedora20-post_install.sh
@@ -61,3 +61,21 @@ sudo yum -y install vim
 
 # JavaHL
 sudo yum -y install subversion-javahl
+
+## java ##
+alternatives --install /usr/bin/java java /usr/java/latest/jre/bin/java 200000
+## javaws ##
+alternatives --install /usr/bin/javaws javaws /usr/java/latest/jre/bin/javaws 200000
+ 
+## Java Browser (Mozilla) Plugin 32-bit ##
+alternatives --install /usr/lib/mozilla/plugins/libjavaplugin.so libjavaplugin.so /usr/java/latest/jre/lib/i386/libnpjp2.so 200000
+ 
+## Java Browser (Mozilla) Plugin 64-bit ##
+alternatives --install /usr/lib64/mozilla/plugins/libjavaplugin.so libjavaplugin.so.x86_64 /usr/java/latest/jre/lib/amd64/libnpjp2.so 200000
+ 
+## Install javac only if you installed JDK (Java Development Kit) package ##
+alternatives --install /usr/bin/javac javac /usr/java/latest/bin/javac 200000
+alternatives --install /usr/bin/jar jar /usr/java/latest/bin/jar 200000
+
+# Add to /etc/profile
+export JAVA_HOME="/usr/java/latest"
